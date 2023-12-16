00c579d69d0f3db22717787315787e5b9698ddb6
Alwaysgone <ffxiizocker@hotmail.com>
2023-12-16 19:22:47 +0100

    Added WIP solution for day05 part02

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -7,6 +7,10 @@ fn main() {
         Ok(lowest_location_number) => println!("lowest location number part01: {}", lowest_location_number),
         Err(e) => println!("an error occurred in part01: {}", e),
     }
+    match part02() {
+        Ok(lowest_location_number) => println!("lowest location number part02: {}", lowest_location_number),
+        Err(e) => println!("an error occurred in part01: {}", e),
+    }
 }
 
 struct Mappings {
@@ -106,4 +110,46 @@ fn parse_mapping(lines:&mut Lines<BufReader<File>>) -> Vec<(u64,u64,u64)> {
                     , splitted_mapping.next().unwrap().parse().unwrap()));
     }
     mapping
+}
+
+fn part02() -> Result<u64>{
+    let path = Path::new("./input/input.txt");
+    let file = match File::open(&path) {
+        Ok(file) => file,
+        Err(e) => panic!("could not open input file: {}", e),
+    };
+    let mut lines = BufReader::new(file).lines();
+    let seeds = lines.next().unwrap()?;
+    let mut splitted_seeds = seeds.split(':');
+    splitted_seeds.next();
+    let mut splitted_seed_numbers = splitted_seeds.next().unwrap().trim().split_whitespace();
+    let mut seed_ranges = vec!();
+    while let Some(seed_range_start_str) = splitted_seed_numbers.next() {
+        let seed_range_start:u64 = seed_range_start_str.parse().unwrap();
+        let seed_range:u64 = splitted_seed_numbers.next().unwrap().parse().unwrap();
+        seed_ranges.push(seed_range_start..seed_range_start+seed_range);
+    }
+    lines.next().unwrap()?;
+    let seed_to_soil = parse_mapping(&mut lines);
+    let soil_to_fertilizer = parse_mapping(&mut lines);
+    let fertilizer_to_water = parse_mapping(&mut lines);
+    let water_to_light = parse_mapping(&mut lines);
+    let light_to_temperature = parse_mapping(&mut lines);
+    let temperature_to_humidity = parse_mapping(&mut lines);
+    let humidity_to_location = parse_mapping(&mut lines);
+    
+    let mappings = Mappings {
+        seed_to_soil,
+        soil_to_fertilizer,
+        fertilizer_to_water,
+        water_to_light,
+        light_to_temperature,
+        temperature_to_humidity,
+        humidity_to_location,
+    };
+    let lowest_location_number = seed_ranges.into_iter()
+    .flat_map(|sr| sr.map(|s| mappings.get_location(s)))
+    .min();
+
+    Ok(lowest_location_number.unwrap())
 }
